988158a6d89eccfe86ac088cd35af6f7bcab4402
Tilak Maddy <tilak.madichetti@gmail.com>
2020-07-10 16:28:06 +0530

    finished the header component (basic)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Route } from 'react-router-dom'
-
+import { Route, Switch } from 'react-router-dom'
 import { HomePage } from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/header.component';
 import './App.css';
 
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={ HomePage }/>
-      <Route path='/shop' component={ ShopPage } />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={ HomePage }/>
+        <Route path='/shop' component={ ShopPage } />
+      </Switch>
     </div>
   );
 }
