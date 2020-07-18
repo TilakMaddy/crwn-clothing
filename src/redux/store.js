83b3c13af73b818762ee678accdfc98eac71da69
Tilak Maddy <tilak.madichetti@gmail.com>
2020-07-18 14:49:41 +0530

    redux logger ↘️ removed for  ∏ mode ENV

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,11 @@ import { persistStore } from 'redux-persist';
 /**
  * as an alternative you caould use redux-dev tools which is 80x sexier
  */
-const middlewares = [ logger ];
+const middlewares = [ /* filled only in development mode */ ];
+
+if(process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
