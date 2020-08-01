1f78f96be327c6a1a8f5065adaaa61e40f2e6566
Tilak Maddy <tilak.madichetti@gmail.com>
2020-08-01 19:46:24 +0530

    reverted accidental change

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 
@@ -22,5 +21,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorker.register();
+
 
