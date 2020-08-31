5093d9359c3415b2fd8d71bbe6fb449159631cc6
Tilak Maddy <tilak.madichetti@gmail.com>
2020-08-31 19:41:37 +0530

    PWA support

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 
@@ -21,5 +22,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-
+serviceWorker.register();
 
